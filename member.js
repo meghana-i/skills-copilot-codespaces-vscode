2476f2fd6a54d1a0e4b9c9678b97e814d96b9bfe
Meghana <94061968+meghana-i@users.noreply.github.com>
2025-03-11 12:06:17 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember()
+{
+    var skills = ["JavaScript", "React", "React Native", "Node", "MongoDB"];
+    var skills = skills.map(skill => skill.toUpperCase());
+    return skills;
+}
